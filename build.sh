66c12c24aae6f9f63c428a908323bc3ea64a282f
Eric Hansen <ehansen@securityfor.us>
2011-11-04 21:56:28 -0400

    Improved files.

--- build.sh
+++ build.sh
@@ -2,10 +2,6 @@
 
 rm -rvf server client
 
-SOURCES="random.cpp vc.cpp"
-ZCLIBS="-lenet"
-ZSCLIBS="-lenet -lshadow -lcrypt"
-
 echo "Compiling program..."
 g++ -g -o client client.cpp Socket.cpp ClientSocket.cpp random.cpp vc.cpp
 g++ -g -o server server.cpp Socket.cpp ServerSocket.cpp auth.cpp random.cpp vc.cpp -lcrypt -lshadow -lpthread
